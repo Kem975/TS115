94bc5f85212bc107b370bf14d759666159f7a961
GOICOECHEA Kemen <Kemen.Goicoechea@bordeaux-inp.fr>
2020-05-24 18:47:38 +0200

    Partie 1 VA gaussienne univariee

---
+++ VA_gaussienne_univariee.m
@@ -0,0 +1,23 @@
+clear; clc; close all;
+
+%Variables
+N = 10001;
+et = 20;
+sigma = sqrt(9);
+mu=2;
+
+%Calcul des fonctions
+v_a = mu + sigma*randn(N,1);
+x = -20:0.004:20;
+f_x = exp((-1/2)*((x-mu)/sigma).^2)*1/(sqrt(2*pi)*sigma);
+
+%Manipulation 1
+figure,
+plot(x,N*f_x);
+hold on
+histogram(v_a,et) %La fonction histogram a plus bel affichage que la fonction hist, donc plus pratique pour relancer le script plusieurs fois
+hold off
+
+%Manipulation 2
+[iteration, axis] = hist(v_a, et,x); %La fonction hist a l'avantage de retourner ses valeurs par rapport à la fonction histogram
+entropy(iteration,axis)
---
+++ entropy.m
@@ -0,0 +1,12 @@
+function H = entropy(iteration, axis)
+    sum=0;
+    proba=zeros(1,20); %Pas obligatoire mais utile pour récupérer les valeurs des probas pour vérifier
+    for i=1:1:20;
+        proba(i) = iteration(i)/10001;
+        sum = sum + proba(i)*log(proba(i));
+    end
+    
+    H = -sum;
+    
+end
+
